8004d93a326f987f6d4ce23baa7f6ca894e1c85b
Oded Hellman <Oded.Hellman@gmail.com>
2020-09-14 01:41:59 +0300

    Integrate product data from db into the homepage

--- superx/routes/home.py
+++ superx/routes/home.py
@@ -1,7 +1,25 @@
 from flask import render_template
- 
+
+from models import *
+
+
 def home():
-    return render_template('home.html')
+    products = db.session.query(Product).all()
+    return render_template('home.html', products=products)
+
 
 def login():
-	return render_template('login.html')
+    return render_template('login.html')
+
+# Snippet for creating data and pushing to db, use in the right context
+# db.create_all()
+#     db.session.commit()
+#
+#     products = [
+#         Product(name='תפוח', quantity=20, is_weighted=True),
+#         Product(name='תפוח', quantity=20, is_weighted=True),
+#         Product(name='תפוח', quantity=20, is_weighted=True),
+#     ]
+#
+#     db.session.add_all(products)
+#     db.session.commit()
