1525a5ed4eaf289e2b7aad5fd0a443a0a4303f07
Oded Hellman <Oded.Hellman@gmail.com>
2020-09-14 01:39:33 +0300

    Add pymysql package

--- superx/config.py
+++ superx/config.py
@@ -4,5 +4,5 @@ import os
 class BaseConfig:
     # the second argument is default DBURI and can be removed
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI",
-                                             "mysql://d5OJkYqkNz:3Z505SacGZ@remotemysql.com/d5OJkYqkNz")
+                                             "mysql+pymysql://d5OJkYqkNz:3Z505SacGZ@remotemysql.com/d5OJkYqkNz")
 
