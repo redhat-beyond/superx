2ec8bf3b65a0b8e679e7ee1a7f685fb34a822822
Oded Hellman <Oded.Hellman@gmail.com>
2020-09-23 19:38:34 +0300

    Update the SQL server URI

--- superx/config.py
+++ superx/config.py
@@ -4,5 +4,5 @@ import os
 class BaseConfig:
     # the second argument is default DBURI and can be removed
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI",
-                                             "mysql+pymysql://Yet43xfTxy:HPt04GGcvA@remotemysql.com/Yet43xfTxy")
+                                             "mysql+pymysql://Super_User:SuperX1234@mysql-13101-0.cloudclusters.net:13101/SuperX")
 
