c9ff4849873a88bc20c3a365307b19c19714c0de
aryehlev <59808808+aryehlev@users.noreply.github.com>
2020-09-30 13:06:04 +0300

    earse

--- superx/routes/home.py
+++ superx/routes/home.py
@@ -7,12 +7,3 @@ def home():
 
     products = Product.query.all()
     return render_template('home.html', products=products)
-
-
-# def logged_in():
-   
-#     basket = db.session.query(Basket).get(1)
-
-#     return render_template('home-signedin.jinja2', basket=basket)
-
-    
