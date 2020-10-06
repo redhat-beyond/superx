aa92d4137afafe81c4703c6ef6b1a3591adac674
Yoav <68295608+BenHurCreations@users.noreply.github.com>
2020-10-07 01:11:11 +0300

    good

---
+++ superx/routing.py
@@ -0,0 +1,39 @@
+from app import app
+from routes import home, signup
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return home.home()
+
+
+@app.route("/livesearch", methods=["POST", "GET"])
+def livesearch():
+    return home.livesearch()
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return signup.login()
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return signup.register()
+
+
+@app.route('/inhome', methods=['GET', 'POST'])
+def logged_in():
+    return home.logged_in()
+
+
+@app.route('/cart/<items_list>', methods=['GET', 'POST'])
+def cart(items_list):
+    return home.cart(items_list)
+
+
+@app.route("/logout")
+def logout():
+    return signup.logout()
+
