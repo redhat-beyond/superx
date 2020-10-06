891419b4c57b8da803494e3068643b065b0a02a0
Yoav <68295608+BenHurCreations@users.noreply.github.com>
2020-10-07 01:06:47 +0300

    good

--- superx/superx/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-class BaseConfig:
-    # the second argument is default DBURI and can be removed
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI",
-                                             "mysql+pymysql://Super_User:SuperX1234@mysql-13101-0.cloudclusters.net:13101/SuperX")
-
