03ea2994fb7d228b49a158b06e79f5894fa7f5aa
Oded Hellman <Oded.Hellman@gmail.com>
2020-10-09 17:26:41 +0300

    String manipulation to transfer the url into list of items code

--- superx/routing.py
+++ superx/routing.py
@@ -2,7 +2,6 @@ from app import app
 from routes import home, signup
 
 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return home.home()
@@ -28,12 +27,20 @@ def logged_in():
     return home.logged_in()
 
 
-@app.route('/cart/<items_list>', methods=['GET', 'POST'])
-def cart(items_list):
-    return home.cart(items_list)
+@app.route('/cart/<string:items_code_string>', methods=['GET', 'POST'])
+def cart(items_code_string):
+    # convert string of items_code to list of items_code
+    items_code_list = []
+    one_product_string = ''
+    for char in items_code_string:
+        if char == '$':
+            items_code_list.append(int(one_product_string))
+            one_product_string = ''
+            continue
+        one_product_string += char
+    return home.cart(items_code_list)
 
 
 @app.route("/logout")
 def logout():
     return signup.logout()
-
