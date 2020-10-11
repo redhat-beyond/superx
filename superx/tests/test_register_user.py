8b0a7198884019a857b5e9acb228846d2c0fb670
aryehlev <59808808+aryehlev@users.noreply.github.com>
2020-10-11 23:40:45 +0300

    test-register

---
+++ superx/tests/test_register_user.py
@@ -0,0 +1,24 @@
+from app import db
+from models import User
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+def test_register_user(client):
+    
+    test_user = {
+        'username': 'Admin',
+        'email': 'aryehlevklein@gmail.com',
+        'password' : '12345678',
+        'submit': 'צור משתמש חדש'
+    }
+    # Register new user
+    client.post('/register', data=test_user, follow_redirects=True)
+
+    validate_user = User.query.filter(User.email == 'aryehlevklein@gmail.com').first()
+    assert validate_user.name == 'Admin'
+    assert validate_user.email == 'aryehlevklein@gmail.com'
+    # assert validate_user.password == check_password_hash(validate_user.password, generate_password_hash('12345678', method='sha256'))
+    User.query.filter(User.id == validate_user.id).delete()
+    db.session.commit()
+    User.query.filter(User.id == validate_user.id).delete()
+    db.session.commit()
