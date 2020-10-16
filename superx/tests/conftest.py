4cd0fe1777fbd158cd23faa25463724619ca46ef
aryehlev <59808808+aryehlev@users.noreply.github.com>
2020-10-16 08:19:37 +0300

    by mistake erase- putting back

--- superx/tests/conftest.py
+++ superx/tests/conftest.py
@@ -1,9 +1,11 @@
 import pytest
 from app import app
-
+from app import db
+from models import User
 
 @pytest.fixture
 def client():
     app.config['WTF_CSRF_ENABLED'] = False
     app.config['TESTING'] = True
     return app.test_client()
+
---
+++ superx/tests/test_login_user.py
@@ -0,0 +1,29 @@
+from app import db
+from models import User
+from werkzeug.security import  generate_password_hash
+from flask import url_for, request
+import pytest
+
+@pytest.mark.run(order=2)
+def test_login_user(client):
+    
+    fake_user = {
+        'email': 'fake@fake.com',
+        'password' : '12345678',
+    }
+    response = client.post('/login', data=fake_user, follow_redirects=True)
+    assert b'*Email does not exist*' in response.data
+    
+    hash_password = generate_password_hash('12345678', method='sha256')
+    
+    test_user = {
+        'email': 'aryehlevklein@gmail.com',
+        'password' : '12345678',
+    }
+    user = User.query.filter(User.email == 'aryehlevklein@gmail.com').first()
+    
+    
+    client.post('/login', data=test_user, follow_redirects=True)
+    
+    assert user.is_authenticated == True
+
