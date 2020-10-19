2a3f8ca5299c5d2c942d2291418693302779650f
aryehlev <59808808+aryehlev@users.noreply.github.com>
2020-10-19 18:47:05 +0300

    config database correctly

--- superx/config.py
+++ superx/config.py
@@ -9,6 +9,5 @@ class BaseConfig: #pylint: disable=too-few-public-methods
     configuring database to attach to remote SQL
     '''
     # the second argument is default DBURI and can be removed
-    SQLALCHEMY_DATABASE_URI = os.environ.get("""SQLALCHEMY_DATABASE_URI",
-                    "mysql+pymysql://
-                    Super_User:SuperX1234@mysql-13101-0.cloudclusters.net:13101/SuperX""")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI",
+    "mysql+pymysql://Super_User:SuperX1234@mysql-13101-0.cloudclusters.net:13101/SuperX")
