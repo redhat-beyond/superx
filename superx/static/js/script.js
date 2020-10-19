45bb474c61cf8ae135d8fd591e9179f3f00bfc75
Yoav <68295608+BenHurCreations@users.noreply.github.com>
2020-10-19 13:40:07 +0300

    Added condition to prevent user from inserting the same product twice

--- superx/static/js/script.js
+++ superx/static/js/script.js
@@ -1,5 +1,25 @@
 // JavaScripts functions
 
+// Check if item is already in cart
+function addItemIfInCart(product_id, product_name){
+  //test 1- console.log("yes");
+  let b = false;
+  let table = $('#cartbody');
+  //test 2- console.log(table);
+  if(table != null){
+    //test 3- console.log(table[0].rows.length);
+    for(let i = 0; i < table[0].rows.length; i++) {
+      let id = table[0].rows[i].id;
+      //test 4- console.log(id);
+      if(id == product_id)
+        b = true;
+    }
+  }
+  //test 5- console.log(b);
+  if(!b)
+    addItem(product_id, product_name);
+}
+
 // adding Items from "myTable" table to "mycart" table
 function addItem(product_id, product_name){
   $.ajax({
@@ -7,18 +27,18 @@ function addItem(product_id, product_name){
     method: "POST",
     data: {id: product_id, name: product_name},
     success: function (res) {
+      console.log("did it2");
       const tableBody = $('#cartbody');
       tableBody.append($(`<tr id="${product_id}">
-          <td colspan="2">${product_id}</td>
-          <td colspan="2">${product_name}</td>
-          <td><button onclick="removeItem(${product_id})" type="button" class="btn btn-outline-danger">הסר מהעגלה</button></td>
-          <td colspan="0"><input type="hidden" name="${product_id}"></td>
+        <td colspan="2">${product_id}</td>
+        <td colspan="2">${product_name}</td>
+        <td><button onclick="removeItem(${product_id})" type="button" class="btn btn-outline-danger">הסר מהעגלה</button></td>
+        <td colspan="0"><input type="hidden" name="${product_id}"></td>
             </tr>`));
       $('#comperbutton').removeAttr('disabled');
     }
   })
 }
-
 
 // function that removes table row and if there are no items in cart - disabled 'comparebutton' button
 function removeItem(product_id) {
