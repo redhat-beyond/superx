1aa01876091e89a97d0af74521f38e8a48fb8150
BenHurCreations <yoavbenhur@gmail.com>
2020-10-25 23:06:16 +0200

    homepage testing

---
+++ superx/tests/test_add_item.py
@@ -0,0 +1,22 @@
+'''
+import client session
+'''
+import pytest
+from flask import session
+
+
+@pytest.mark.run(order=4)
+def test_add_item(client):
+    test_item = {
+        'id': '123123123',
+        'name': 'ananas',
+        'quantity': '1',
+        'is_weighted': 'True',
+        'unit_of_measure': 'קילו'
+    }
+
+    with client:
+        client.post('/addItem', data=test_item, follow_redirects=True)
+        item = session['cart'][-1]
+        assert item["id"] == test_item["id"]
+        assert item["name"] == test_item["name"]
