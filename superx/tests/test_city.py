8796e9405bcd77a23642f6cb3492113fefc50e2a
aryehlev <59808808+aryehlev@users.noreply.github.com>
2020-11-07 12:01:45 +0200

    city test

---
+++ superx/tests/test_city.py
@@ -0,0 +1,18 @@
+'''
+import User from models and pytest and database
+'''
+import pytest
+from flask import session 
+
+
+@pytest.mark.run(order=7)
+def test_choose_city(client):
+    '''
+    tests add city
+    '''
+    test_city = {
+        'city' : 'ירושלים'
+    }
+    with client:
+        client.post('/city', data=test_city, follow_redirects=True)
+        assert session['city'] is 'ירושלים' 
